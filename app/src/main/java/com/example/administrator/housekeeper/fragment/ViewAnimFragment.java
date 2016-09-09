3704122f989d4161ea5de6468f8d07dcd5ec923b
JZheng-android <591513550@qq.com>
2016-09-09 13:59:10 +0800

    anim

---
+++ app/src/main/java/com/example/administrator/housekeeper/fragment/ViewAnimFragment.java
@@ -0,0 +1,32 @@
+package com.example.administrator.housekeeper.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.administrator.housekeeper.R;
+/**
+ * Created by Administrator on 2016/9/8 0008.
+ */
+public class ViewAnimFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_picture, null);
+        final ImageView iv_hzw = (ImageView) view.findViewById(R.id.iv_a1);
+
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.set_anim);
+        iv_hzw.startAnimation(animation);
+
+//        iv_hzw.setAnimation(animation);
+//        animation.start();
+        return view;
+    }
+}
